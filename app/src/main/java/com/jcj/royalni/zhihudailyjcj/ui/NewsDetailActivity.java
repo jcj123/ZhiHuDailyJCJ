2beca4ef5242dd73d3cea912df9be913edf3173c
jcj123 <jcjkobe1991@163.com>
2017-07-14 16:39:09 +0800

    第三次提交 完成了基本的详情页显示功能（后续的分享等功能还需要进一步优化）

--- app/src/main/java/com/jcj/royalni/zhihudailyjcj/ui/NewsDetailActivity.java
+++ app/src/main/java/com/jcj/royalni/zhihudailyjcj/ui/NewsDetailActivity.java
@@ -4,13 +4,20 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.jcj.royalni.zhihudailyjcj.Obversable.ObservableFromHttp;
 import com.jcj.royalni.zhihudailyjcj.R;
 import com.jcj.royalni.zhihudailyjcj.bean.NewsDetail;
 import com.jcj.royalni.zhihudailyjcj.bean.Story;
+import com.jcj.royalni.zhihudailyjcj.utils.HtmlUtil;
 
 import butterknife.Bind;
 import rx.Scheduler;
@@ -31,6 +38,13 @@ public class NewsDetailActivity extends BaseActivity {
     TextView mTvDetail;
     @Bind(R.id.tv_image_from)
     TextView mTvImageFrom;
+    @Bind(R.id.toolbar)
+    Toolbar mToolbar;
+    @Bind(R.id.collapsing_toolbar_layout)
+    CollapsingToolbarLayout mCollapsingToolbarLayout;
+    @Bind(R.id.detail_webview)
+    WebView mDetailWebview;
+    //通过这种方式启动activit十分科学，可以让其他意图启动这个activity的人知道该传入什么参数进来
     public static void startNewsDetailActivity(Context context, Story story) {
         Intent intent = new Intent(context,NewsDetailActivity.class);
         intent.putExtra(NEWS_KEY,story);
@@ -41,6 +55,13 @@ public class NewsDetailActivity extends BaseActivity {
     protected void afterCreate(Bundle savedInstanceState) {
         Intent intent = getIntent();
         mStory = intent.getParcelableExtra(NEWS_KEY);
+        setSupportActionBar(mToolbar);
+        ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        mCollapsingToolbarLayout.setTitleEnabled(true);
         showData(mStory);
     }
 
@@ -53,8 +74,30 @@ public class NewsDetailActivity extends BaseActivity {
                     @Override
                     public void call(NewsDetail newsDetail) {
                         mTvDetail.setText(newsDetail.getTitle());
+                        System.out.println("newsDetail.getImage_source())="+newsDetail.getImage_source());
+                        mTvImageFrom.setText(newsDetail.getImage_source());
+                        Glide.with(NewsDetailActivity.this).load(newsDetail.getImage()).into(mDetailIv);
+
+                        mDetailWebview.setDrawingCacheEnabled(true);
+                        String htmlSource = HtmlUtil.createHtmlData(newsDetail,false);
+                        //这样显示会在格式上出现一点问题
+//                        mDetailWebview.loadDataWithBaseURL(null, htmlSource,"text/html", "utf-8", null);
+                        mDetailWebview.loadData(htmlSource,HtmlUtil.MIME_TYPE,HtmlUtil.ENCODING);
                     }
                 });
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mDetailWebview != null) {
+            mDetailWebview.loadDataWithBaseURL(null,"","text/html","utf-8",null);
+            mDetailWebview.clearHistory();
+            ((ViewGroup)mDetailWebview.getParent()).removeView(mDetailWebview);
+            mDetailWebview.destroy();
+            mDetailWebview = null;
+        }
+        super.onDestroy();
+
     }
 
     @Override
